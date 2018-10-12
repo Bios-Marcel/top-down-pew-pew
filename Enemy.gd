06d33f6288341bf924fb82ae85bee516032e1e63
Codeah <codeah@protonmail.com>
2018-10-12 21:46:28 +0300

    Improved entity damaging

--- Enemy.gd
+++ Enemy.gd
@@ -6,3 +6,6 @@ func _entity_damaged(amount):
 func _entity_died():
 	print("You killed someone! You absolute MONSTER!")
 	queue_free()
+
+func get_shot():
+	damage(15)
--- bullet.gd
+++ bullet.gd
@@ -5,3 +5,9 @@ const bulletSpeed = 10
 func _physics_process(delta):
 	move_local_x(bulletSpeed)
 	pass
+
+
+func _on_Bullet_body_entered(body):
+	if body.has_method("get_shot"):
+		body.get_shot()
+	queue_free()
