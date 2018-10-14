12c694fca1142422d2b5585cf01edcbd966529e4
Bios-Marcel <marceloschr@googlemail.com>
2018-10-14 17:25:08 +0200

    Add Resume and exit button

--- interface/Pause/Pause.gd
+++ interface/Pause/Pause.gd
@@ -2,7 +2,23 @@ extends Control
 
 func _input(event):
 	if event.is_action_pressed("ui_pause"):
-		var new_pause_state = not get_tree().paused
-		get_tree().paused = new_pause_state
+		toggle_pause()
 
-		visible = new_pause_state
+	if event.is_action_pressed("ui_down"):
+		if $ResumeButton.has_focus():
+			$ExitButton.grab_focus()
+		elif $ExitButton.has_focus():
+			$ResumeButton.grab_focus()
+	elif event.is_action_pressed("ui_up"):
+		if $ResumeButton.has_focus():
+			$ExitButton.grab_focus()
+		elif $ExitButton.has_focus():
+			$ResumeButton.grab_focus()
+
+func toggle_pause():
+	var new_pause_state = not get_tree().paused
+	get_tree().paused = new_pause_state
+
+	visible = new_pause_state
+	if visible:
+		$ResumeButton.grab_focus()
