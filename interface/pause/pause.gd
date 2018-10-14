1a52693784ea4357f7260c58fbc3d40b92e51062
Bios-Marcel <marceloschr@googlemail.com>
2018-10-14 17:48:06 +0200

    Remove focus code and do it the proper way instead!

--- interface/pause/pause.gd
+++ interface/pause/pause.gd
@@ -3,20 +3,6 @@ extends Control
 func _input(event):
 	if event.is_action_pressed("ui_pause"):
 		toggle_pause()
-	else:
-		handle_focus(event)
-
-func handle_focus(event):
-	if event.is_action_pressed("ui_down"):
-		if $ResumeButton.has_focus():
-			$ExitButton.grab_focus()
-		elif $ExitButton.has_focus():
-			$ResumeButton.grab_focus()
-	elif event.is_action_pressed("ui_up"):
-		if $ResumeButton.has_focus():
-			$ExitButton.grab_focus()
-		elif $ExitButton.has_focus():
-			$ResumeButton.grab_focus()
 
 func toggle_pause():
 	var new_pause_state = not get_tree().paused
